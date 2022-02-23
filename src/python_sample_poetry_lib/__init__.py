9cc8e6ad71a4d055f2dc83f76364e7958d637663
Rem_ <black.white.red.are.the.best@gmail.com>
2022-02-23 22:31:12 +0900

    feat: コードを追加

---
+++ src/python_sample_poetry_lib/__init__.py
@@ -0,0 +1,5 @@
+# from importlib.metadata import version
+
+from .sub import *
+
+# __version__ = version(__package__)
---
+++ src/python_sample_poetry_lib/sub.py
@@ -0,0 +1,6 @@
+# from python_sample_poetry_lib import __version__
+
+
+def sub():
+    print('Run sub')
+    # print(__version__)
